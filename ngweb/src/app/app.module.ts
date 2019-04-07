cf869af934507c7ff41119cfc81628ea97ef8d8f
MrBandaruk <nbandaruk@gmail.com>
2019-04-07 14:23:24 +0300

    Add home page. Add navbar. Add title service.

--- ngweb/src/app/app.module.ts
+++ ngweb/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './modules/shared.module';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
