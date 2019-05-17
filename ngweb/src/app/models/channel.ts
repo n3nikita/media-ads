c4b5348c4746fbbe1ab818f890372832d6840624
MrBandaruk <nbandaruk@gmail.com>
2019-05-18 00:17:34 +0300

    add channel model

---
+++ ngweb/src/app/models/channel.ts
@@ -0,0 +1,10 @@
+export class Channel {
+    id: number;
+    name: string;
+    link: string;
+    subscribers: number;
+    views: number;
+    image: string;
+
+    // TODO: add connected fields
+}
