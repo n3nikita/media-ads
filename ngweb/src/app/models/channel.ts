78bebb32eff1687a54d9533ee23955c7b51221ad
MrBandaruk <nbandaruk@gmail.com>
2019-05-21 23:23:25 +0300

    Update channel component

--- ngweb/src/app/models/channel.ts
+++ ngweb/src/app/models/channel.ts
@@ -1,9 +1,11 @@
 export class Channel {
     id: number;
     name: string;
+    description: string;
     link: string;
     subscribers: number;
     views: number;
+    err: number;
     image: string;
 
     // TODO: add connected fields
