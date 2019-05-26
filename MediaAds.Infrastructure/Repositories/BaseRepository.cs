bd271e3466b521f41082b84fc2525af27a23c8a3
MrBandaruk <nbandaruk@gmail.com>
2019-05-26 19:18:44 +0300

    update file sructure

---
+++ MediaAds.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MediaAds.Core.Interfaces;
+using MediaAds.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaAds.Infrastructure.Data
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : BaseModel
+    {
+        protected readonly MediaDbContext _db;
+
+        public BaseRepository(MediaDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _db.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            _db.Set<T>().Add(entity);
+            await _db.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _db.Set<T>().Remove(entity);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ MediaAds.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MediaAds.Core.Interfaces;
+using MediaAds.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaAds.Infrastructure.Data
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(MediaDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<User> GetUserByCredentials(string username, string password)
+        {
+            var user = await _db.Users.Include(r => r.Role)
+                                      .FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+
+            //if (user is null)
+                //throw new ArgumentException($"User {username}:{password} not found");
+
+            return user;
+        }
+    }
+}
